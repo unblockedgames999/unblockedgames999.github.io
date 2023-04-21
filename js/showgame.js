c9e12a99ff997f3cde3bd030c4b02796b713666b
unblockedgames999 <130808782+unblockedgames999@users.noreply.github.com>
2023-04-22 06:49:43 +0700

    Update showgame.js

--- js/showgame.js
+++ js/showgame.js
@@ -1,5 +1,5 @@
 
-var cont="<a href='" +urls[randomgame]+ "'><img src='" + images[randomgame] + "'border=0 width=274.285714286 height=270></a>"
+var cont="<a href='" +urls[randomgame]+ "'><img src='" + images[randomgame] + "'border=0 width=274 height=270></a>"
 document.write(cont)
 /*
      FILE ARCHIVED ON 17:49:15 May 20, 2018 AND RETRIEVED FROM THE
